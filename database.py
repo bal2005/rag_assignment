cd3fb4ab631a1d867ff1370ca7b8cd9f04fe37f2
Bala <balavan2005@gmail.com>
2026-02-19 18:53:55 +0530

    first commit

---
+++ database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine, text
+import os
+from dotenv import load_dotenv
+from urllib.parse import quote_plus
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")  # encodes special characters
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+print(DATABASE_URL)  # Optional: for testing (remove in production)
+
+
+engine = create_engine(DATABASE_URL)
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM contracts"))
+    for row in result:
+        print(row)
+
