56ffbee061610ed5919b2c48102e7c74dc4a2d7b
M Balasubramanian <121662738+bal2005@users.noreply.github.com>
2026-02-22 14:05:02 +0530

    Update RAG

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from urllib.parse import quote_plus
 load_dotenv()
 
 DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")  # encodes special characters
+DB_PASSWORD = os.getenv("DB_PASSWORD") # encodes special characters
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
